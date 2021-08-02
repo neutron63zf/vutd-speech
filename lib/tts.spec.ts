a08b1a684321b3a94585c38fc6233bb8753e574b
Nkowne63 <neutron63zf@gmail.com>
2021-08-02 15:29:45 +0900

    add: audio synthesize

--- lib/tts.spec.ts
+++ lib/tts.spec.ts
@@ -1,3 +1,12 @@
+import fs from "fs";
+import { synthesize } from "./tts";
+
 describe("tts", () => {
-  test.todo("synthesize");
+  it("save audio to file", async () => {
+    const audioBase64 = await synthesize("こんにちは");
+    fs.writeFileSync(
+      "./test-audio.tmp.mp3",
+      Buffer.from(audioBase64, "base64")
+    );
+  });
 });
--- lib/tts.ts
+++ lib/tts.ts
@@ -1,2 +1,8 @@
+import { getAudioBase64 } from "google-tts-api";
+
+const lang = "ja";
+
 // 引数に応じて音声を合成する
-export const synthesize = () => {};
+export const synthesize = async (text: string) => {
+  return getAudioBase64(text, { lang });
+};
